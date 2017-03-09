a1d9d858b06c05f98bec3144c83f1ac584088b5d
Sol Jennings <srjen3@student.monash.edu>
2017-03-09 20:43:57 +1100

    wawa

--- encode.c
+++ encode.c
@@ -1,16 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <memory.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_IMAGE_DEPTH 255
+#define MAX 512
 
 int getNextCharacter(FILE *fp);
 void printIntAsBinary(int c);
-void processFile(char *filename);
+void processFile(char *filename, char *message);
 void errorAndExit(char error[]);
-void processMessage(char *message);
 enum ImageType getImageType(FILE *file);
-void skipImageComments(FILE *file);
-struct ImageSize getImageSize(FILE *file);
+int moveOverAndCountComments(FILE *file);
+struct ImageInfo getImageInfo(FILE *file);
 void moveFileToNextLine(FILE *file);
+int scanDimension(FILE *file);
+
+bool doesMessageFit(struct ImageInfo info, char *message);
+
+void encodeImage(FILE *file, struct ImageInfo info, int numComments, char *message);
+
+void copyHeader(FILE *file, FILE *outfile, int comments);
 
 enum ImageType {
     unsupported,
@@ -18,9 +28,10 @@ enum ImageType {
     bmp
 };
 
-struct ImageSize {
+struct ImageInfo {
     int width;
     int height;
+    int depth;
 };
 
 int main(int argc, char *argv[]) {
@@ -34,16 +45,9 @@ int main(int argc, char *argv[]) {
     imageFile = argv[1];
     secretMessage = argv[2];
 
-    processFile(imageFile);
-  //  processMessage(secretMessage);
+    processFile(imageFile, secretMessage);
 
     return 0;
-}
-
-void processMessage(char *message) {
-    for (int i=0; i <= sizeof(message); i++) {
-        printIntAsBinary(message[i]);
-    }
 }
 
 enum ImageType getImageType(FILE *file) {
@@ -55,14 +59,13 @@ enum ImageType getImageType(FILE *file) {
         imageType = ppm;
     }
 
-    while(getc(file) != '\n');
-
     return imageType;
 }
 
-void processFile(char *filename) {
+void processFile(char *filename, char *message) {
     int c;
-    struct ImageSize imageSize;
+    int numComments;
+    struct ImageInfo imageSize;
 
     FILE *file = fopen(filename, "r");
 
@@ -74,56 +77,139 @@ void processFile(char *filename) {
         errorAndExit("Image type unsupported");
     }
 
-    skipImageComments(file);
-    imageSize = getImageSize(file);
+    numComments = 0;
 
-    printf("width %d", imageSize.width);
-    printf("height %d", imageSize.height);
+    imageSize = getImageInfo(file);
 
-    while((c = getNextCharacter(file)) != '\n') {
-        printf("%c", c);
-        //printIntAsBinary(c);
+    if (imageSize.depth != MAX_IMAGE_DEPTH) {
+        errorAndExit("Image colour depth must be 255");
     }
+
+    if (!doesMessageFit(imageSize, message)) {
+        errorAndExit("Message does not fit image");
+    }
+
+    printf("width %d\n", imageSize.width);
+    printf("height %d\n", imageSize.height);
+    printf("depth %d\n", imageSize.depth);
+
+    encodeImage(file, imageSize, numComments, message);
 
     fclose(file);
 }
 
-void skipImageComments(FILE *file) {
-    int currentChar;
+void encodeImage(FILE *file, struct ImageInfo info, int numComments, char *message) {
+    FILE *outfile = fopen("outfile.ppm", "w+");
+    copyHeader(file, outfile, numComments);
+    int messageLength = (int) strlen(message) + 1;
 
+    for (int i=0; i < messageLength; i++) {
+        for (int j = 0; j < 8; j++) {
+            printf("%d", (message[i] << j & 0x80)/0x80) ;
+        }
+        printf("\n");
+    }
+
+    fclose(outfile);
+}
+
+void copyHeader(FILE *file, FILE *outfile, int comments) {
+    char line[MAX];
+    int linesCopied=0;
+    rewind(file);
+
+    while (fgets(line,sizeof(line),file) != NULL && linesCopied < comments + 3)
+    {
+        /*Write the line */
+        fputs(line, outfile);
+        linesCopied++;
+    }
+}
+
+bool doesMessageFit(struct ImageInfo info, char *message) {
+
+    if (strlen(message) > sizeof(int) * 8) {
+        return false;
+    }
+
+    int length = (int) strlen(message) + 1;
+
+    return (length * 8) < (info.height * info.width * 3);
+}
+
+int moveOverAndCountComments(FILE *file) {
+    int currentChar;
+    int count = 0;
     while ((currentChar = getc(file)) == '#') {
+        count++;
         moveFileToNextLine(file);
     }
 
     ungetc(currentChar, file);
+
+    return count;
 }
 
-struct ImageSize getImageSize(FILE *file) {
-    struct ImageSize imageSize;
+struct ImageInfo getImageInfo(FILE *file) {
+    struct ImageInfo imageInfo;
+    bool headerComplete = false;
+    int nextChar;
+    int currentDimension = 0;
+    int dimensionValue;
 
-    fscanf(file, "%d %d", &imageSize.width, &imageSize.height);
-    return imageSize;
-}
+    while (!headerComplete) {
+        nextChar = getc(file);
+        printf("%c\n", nextChar);
+        ungetc(nextChar, file);
+        getchar();
+        if (nextChar == '#') {
+            moveFileToNextLine(file);
+            continue;
+        }
+        dimensionValue = scanDimension(file);
 
-int getNextCharacter(FILE *file) {
-    int c = getc(file);
-    return c;
-}
+        if (dimensionValue > 0) {
+            switch (currentDimension) {
+                case 0:
+                    imageInfo.width = dimensionValue;
+                    currentDimension++;
+                    break;
+                case 1:
+                    imageInfo.height = dimensionValue;
+                    currentDimension++;
+                    break;
+                case 2:
+                    imageInfo.depth = dimensionValue;
+                    headerComplete = true;
+                    break;
+                default:
+                    errorAndExit("wtf");
+            }
 
-void printIntAsBinary(int c) {
-    int i;
-    for (i = 0; i < 8; i++) {
-        printf("%d", ((c << i) & 0x80) != 0);
+        }
+        printf("%d %d %d\n", imageInfo.height, imageInfo.width, imageInfo.depth);
+
     }
 
-    printf("\n");
+    return imageInfo;
 }
+
+int scanDimension(FILE *file) {
+    int dimension;
+    if (fscanf(file, "%d", &dimension) == 0) {
+        return -1;
+    }
+    printf("scanned %d\n", dimension);
+    return dimension;
+}
+
+
 void moveFileToNextLine(FILE *file) {
     while (getc(file) != '\n');
 }
 
 void errorAndExit(char error[]) {
-    printf("Error: %s", error);
+    printf("Error: %s\n", error);
     exit(1);
 }
 
